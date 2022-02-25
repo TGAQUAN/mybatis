e276f1cfad580535a7f077fcfafb42eaf986a4ef
QinZhen <qintga@163.com>
2022-02-25 14:23:24 +0800

    1111111

--- src/main/java/com/mybatis/MybatisApplication.java
+++ src/main/java/com/mybatis/MybatisApplication.java
@@ -10,5 +10,6 @@ public class MybatisApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
     }
+    public void test1(){}
 
 }
